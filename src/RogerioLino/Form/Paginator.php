208e9977705c1794034ee960e046beb21ab90809
Rogério Alencar Lino Filho <rogeriolino@gmail.com>
2014-03-27 10:38:30 -0300

    paginator

---
+++ src/RogerioLino/Form/Paginator.php
@@ -0,0 +1,47 @@
+<?php
+namespace RogerioLino\Form;
+
+/**
+ * BasicValidator
+ *
+ * @author Rogerio Lino <rogeriolino@gmail.com>
+ */
+class BasicValidator implements Validator {
+    
+    private $requireds = array();
+    private $data = array();
+    
+    /**
+     * Set requireds values
+     * @param array $requireds
+     * @return \RogerioLino\Form\BasicValidator
+     */
+    public function requireds(array $requireds) {
+        $this->requireds = $requireds;
+        return $this;
+    }
+    
+    /**
+     * Set the data to be validated
+     * @param array $data
+     * @return \RogerioLino\Form\BasicValidator
+     */
+    public function data(array $data) {
+        $this->data = $data;
+        return $this;
+    }
+
+    /**
+     * @throws Exception
+     * @return boolean
+     */
+    public function isValid() {
+        foreach ($this->requireds as $req) {
+            if (!isset($this->data[$req]) || trim($this->data[$req]) == '') {
+                throw new \Exception(sprintf('Campo obrigatório: %s', $req));
+            }
+        }
+        return true;
+    }
+    
+}
